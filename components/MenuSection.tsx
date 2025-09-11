4c3370ba0524d4202989f4905e08ccac023ce773
knodelchik <ulanovskiy1110@gmail.com>
2025-09-11 23:18:08 +0300

    glob 2

--- components/MenuSection.tsx
+++ components/MenuSection.tsx
@@ -8,11 +8,11 @@ export default function MenuSection() {
   return (
     <section
       id="menu"
-      className="container relative w-full text-white flex flex-col items-center justify-center 
+      className="relative w-full text-white flex flex-col items-center justify-center 
                  bg-gradient-to-b from-[#3B332B] via-[#2c2620] to-[#221e1c] px-8 pb-16"
     >
       {/* Заголовок */}
-      <div className="flex items-center justify-center gap-6 mb-12 w-full-7xl container">
+      <div className="flex items-center justify-center gap-6 mb-12 w-full container">
         <div className="flex-grow h-[2px] bg-[#5a532c] rounded-full" />
         <h2
           className="text-4xl font-bold text-[#c5c18d] text-center whitespace-nowrap"
